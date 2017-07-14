bcff323f8d5d82a18acb861040c6979764a4c274
Evan Agee <evanagee@gmail.com>
2017-07-14 10:23:54 -0500

    gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 // Defining requirements
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const watch = require('gulp-watch');
+const run = require('gulp-run');
+const concat = require('gulp-concat');
+const ngrok = require('ngrok');
 
 const cssFiles = [
   'assets/_sass/**/*.?(s)css', 
@@ -19,15 +22,24 @@ gulp.task('css', () => {
   gulp.src(cssFiles)
     .pipe(sass())
     .pipe(concat('main.css'))
-    .pipe(gulp.dest('assets'))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest('assets'));
 });
 
 gulp.task('scripts', function() {
   gulp.src(jsFiles)
     .pipe(concat('all.js'))
-    .pipe(gulp.dest('assets'))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest('assets'));
 });
 
-gulp.task('default', ['css', 'scripts']);
+gulp.task('rackup', function() {
+  return run('rackup').exec();
+});
+
+gulp.task('ngrok', function() {
+  ngrok.connect({
+    proto: 'http',
+    addr: 9292,
+  }, function (err, url) {});
+});
+
+gulp.task('default', ['css', 'scripts', 'rackup', 'ngrok']);
